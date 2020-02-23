d4ef5488f9d7f6b6a65f0cb1180c92a3cce5e394
viniciusalopes <suporte@viniciusalopes.com.br>
2020-02-23 15:01:00 -0300

    POO, Lista 1, Ex10, ProjetoFuncionario (dom fev 23 15:01:00 -03 2020)

---
+++ poo/lista-de-exercicios-1/ex10/ProjetoFuncionario/src/classes/Funcionario.java
@@ -0,0 +1,94 @@
+package classes;
+
+public class Funcionario {
+
+    // ATRIBUTOS
+    private String nome = "";
+    private float salario = 0;
+    private int filhos = 0;
+
+    // GET
+    public String getNome() {
+        return nome;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public int getFilhos() {
+        return filhos;
+    }
+
+    // SET
+    public void setNome(String nome) throws Exception {
+        if (nome.trim().length() < 2) {
+            throw new Exception("Nome inválido");
+        }
+        this.nome = nome;
+    }
+
+    public void setSalario(float salario) throws Exception {
+        if (salario < 0) {
+            throw new Exception("Salário não pode ser < 0");
+        }
+        this.salario = salario;
+    }
+
+    public void setFilhos(int filhos) throws Exception {
+        if (filhos < 0) {
+            throw new Exception("Quantidade de filhos não pode ser < 0");
+        }
+        this.filhos = filhos;
+    }
+
+    // CÁLCULOS
+    public float getAumentoSalarial(float percent) throws Exception {
+        if (percent <= 0) {
+            throw new Exception("Percentual de aumento não pode ser <= 0");
+        }
+        return (salario * (percent / 100));
+    }
+
+    public float getInssAliquota() {
+        if (salario <= (float) 2545) {
+            return (float) 6;
+        } else {
+            return (float) 10;
+        }
+    }
+
+    public float getInssValor() {
+        return (float) (salario * (getInssAliquota() / 100));
+    }
+
+    public float getIrAliquota() {
+        if (salario < (float) 1903.99) {
+            return 0;
+        } else if (salario < (float) 2826.66) {
+            return (float) 7.5;
+        } else if (salario < (float) 3751.06) {
+            return (float) 15;
+        } else if (salario < (float) 4664.68) {
+            return (float) 22.5;
+        } else {
+            return (float) 27.5;
+        }
+    }
+
+    public float getIrParcela() {
+        return (float) (salario * (getIrAliquota() / 100));
+    }
+
+    public float getIrValorMensal() throws Exception {
+        float valor = (float) (getIrParcela() - (filhos * 545));
+        /* 
+        E agora, José?
+        
+        if (valor < 0) {
+            Mensagem de erro ou valor = 0 ??
+        }
+         */
+        return valor;
+    }
+}
