bd62218e8b27651af3a4ea3a5a830c16e017d7cc
viniciusalopes <suporte@viniciusalopes.com.br>
2020-02-22 21:12:15 -0300

    POO, Lista 1, Ex7, ProjetoCone (sáb fev 22 21:12:14 -03 2020)

---
+++ poo/lista-de-exercicios-1/ex7/ProjetoCone/src/classes/Cone.java
@@ -0,0 +1,66 @@
+package classes;
+
+public class Cone {
+
+    // ATRIBUTOS
+    private int raio = 0;
+    private int altura = 0;
+
+    // GET
+    public int getRaio() {
+        return raio;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    // SET
+    public void setRaio(int raio) throws Exception {
+        if (raio <= 0) {
+            throw new Exception("O raio não pode ser <= 0");
+        }
+        this.raio = raio;
+    }
+
+    public void setAltura(int altura) throws Exception {
+        if (altura <= 0) {
+            throw new Exception("A altura não pode ser <= 0");
+        }
+        this.altura = altura;
+    }
+
+    // CÁLCULOS
+    public float calcularGeriatriz() throws Exception {
+        if (raio <= 0 || altura <= 0) {
+            throw new Exception("Medidas inválidas");
+        }
+        // O cálculo da geratriz é (Raiz Quadrada((altura 2 )+(raio 2 )))
+        return (float) (Math.sqrt((altura * altura) + (raio * raio)));
+    }
+
+    public float calcularAreaLateral() throws Exception {
+        if (raio <= 0 || altura <= 0) {
+            throw new Exception("Medidas inválidas");
+        }
+        // O cálculo da área lateral é (3.1415*raio*geratriz)
+        return (float) (3.1415 * raio * calcularGeriatriz());
+    }
+
+    public float calcularAreaTotal() throws Exception {
+        if (raio <= 0 || altura <= 0) {
+            throw new Exception("Medidas inválidas");
+        }
+        // O cálculo da área total é (3.1415*raio*(geratriz+raio))
+        return (float) (3.1415 * raio * (calcularGeriatriz() + raio));
+    }
+
+    public float calcularVolume() throws Exception {
+        if (raio <= 0 || altura <= 0) {
+            throw new Exception("Medidas inválidas");
+        }
+        // O cálculo do volume é (1.0/3.0*3.1415*raio 2 *altura)
+
+        return (float) (1.0 / 3.0 * 3.1415 * (raio * raio) * altura);
+    }
+}
